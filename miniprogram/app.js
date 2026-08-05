b689cbbf61c7ebc1e0c1c789b17e07128801e59c
Niu717 <15531692785@163.com>
2026-08-05 20:39:25 +0800

    fix: skip cloud initialization in tourist mode

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,11 +1,13 @@
+const USE_CLOUD_REPORTS = false
+
 App({
   onLaunch() {
-    if (wx.cloud) {
+    if (USE_CLOUD_REPORTS && wx.cloud) {
       wx.cloud.init({ traceUser: true })
     }
   },
   globalData: {
     projectName: '机器视觉缺陷检测系统',
-    useCloudReports: false
+    useCloudReports: USE_CLOUD_REPORTS
   }
 })
